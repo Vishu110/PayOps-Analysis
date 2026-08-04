ccfb4cf6c040e08af0e409eef63bfa92b6b4f39c
Vishal US <vishalus1997@gmail.com>
2026-08-04 22:37:50 +0530

    feat(database):initialize project structure and create customers master table

---
+++ README.md
@@ -0,0 +1,20 @@
+# PayOps-Analysis
+
+A production-inspired payment operations platform designed to simulate the operational workflows of modern payment processors.
+
+## Goals
+
+- Learn end-to-end payment flows
+- Simulate realistic payment data
+- Perform reconciliation and operational investigations
+- Build production-quality SQL skills
+- Develop automation using Python
+- Prepare for Payments Analyst and Data Analyst roles
+
+## Tech Stack
+
+- PostgreSQL
+- Python
+- SQL
+- Power BI
+- Pandas
---
+++ database/ddl/001_customers.sql
@@ -0,0 +1,65 @@
+DROP TABLE IF EXISTS customers CASCADE;
+
+DROP TYPE IF EXISTS customer_status_enum CASCADE;
+DROP TYPE IF EXISTS risk_segment_enum CASCADE;
+
+--ENUMS
+CREATE TYPE customer_status_enum as ENUM (
+    'ACTIVE',
+    'BLOCKED',
+    'CLOSED'
+);
+
+CREATE TYPE risk_segment_enum as ENUM (
+    'LOW',
+    'MEDIUM',
+    'HIGH'
+);
+
+
+--TABLE
+CREATE TABLE customers (
+    id BIGSERIAL,
+    customer_id VARCHAR(50) NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20),
+    country_code CHAR(2) NOT NULL,
+    country_name VARCHAR(100) NOT NULL,
+    state VARCHAR(100),
+    city VARCHAR(100),
+    timezone VARCHAR(100) NOT NULL,
+    preferred_currency CHAR(3) NOT NULL,
+    risk_segment risk_segment_enum NOT NULL,
+    customer_status customer_status_enum NOT NULL,
+    signup_date DATE NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    --Constraints
+    CONSTRAINT pk_customers
+    PRIMARY KEY (id),
+
+    CONSTRAINT uq_customers_customer_id
+    UNIQUE (customer_id),
+
+    CONSTRAINT uq_customers_email
+    UNIQUE (email)
+);
+
+
+--Indexes
+CREATE INDEX idx_customers_country
+ON customers(country_code);
+
+CREATE INDEX idx_customers_risk
+ON customers(risk_segment);
+
+CREATE INDEX idx_customers_status
+ON customers(customer_status);
+
+CREATE INDEX idx_customers_signup
+ON customers(signup_date);
+
+u
